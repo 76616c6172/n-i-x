5101400acf26c8bd2edb1240bef77a7c90ad29ae
valar <68338647+76616c6172@users.noreply.github.com>
2022-11-01 21:51:46 -0700

    style(flake.nix): change formatting

--- flake.nix
+++ flake.nix
@@ -1,24 +1,26 @@
 {
   description = "n-i-x is a wrapper for the nix CLI";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  # TODO insert tests
   outputs = { self, nixpkgs }:
     let
-      # Generate a user-friendly version number.
+      # Generate a user-friendly version number
       version = builtins.substring 0 8 self.lastModifiedDate;
-      # System types to support.
+      # System types to support
       supportedSystems = [ "x86_64-linux" ];
-      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      # Nixpkgs instantiated for supported system types.
+      # Nixpkgs instantiated for supported system types
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
     in
     {
-      # Provide some binary packages for selected system types.
+      # Provide some binary packages for selected system types
       packages = forAllSystems (system:
         let
           pkgs = nixpkgsFor.${system};
         in
         {
+          # make the flake (make derivation)
           n-i-x = pkgs.stdenv.mkDerivation {
             pname = "n-i-x"; inherit version;
             src = ./src;
