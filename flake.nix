46d6d656aab47fda06feee1498445c19d60614d3
valar <68338647+76616c6172@users.noreply.github.com>
2022-11-01 22:14:49 -0700

    change(flake.nix): use "build-${lastModifiedDate}" version tag

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
   outputs = { self, nixpkgs }:
     let
       # Generate a user-friendly version number
-      version = builtins.substring 0 8 self.lastModifiedDate;
+      version = "build-${builtins.substring 0 8 self.lastModifiedDate}";
       # System types to support
       supportedSystems = [ "x86_64-linux" ];
       # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'
